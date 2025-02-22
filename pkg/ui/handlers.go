83274e99c89963a7fdb22341fba7fc9648fa28be
Mariusz Księżyk <ksiezykm@gmail.com>
2025-02-22 22:17:07 +0100

    Key bindings start

--- pkg/ui/handlers.go
+++ pkg/ui/handlers.go
@@ -23,7 +23,10 @@ func CursorDown(g *gocui.Gui, v *gocui.View) error {
 		return nil
 	}
 	cx, cy := v.Cursor()
-	return v.SetCursor(cx, cy+1)
+	if cy < 1 {
+		return v.SetCursor(cx, cy+1)
+	}
+	return nil
 }
 
 // Quit exits the application
